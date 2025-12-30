05298103509bfef89b63e75a8e00940cec14debc
AgentForceHR <github@agentforcehr.com>
2025-12-30 15:50:33 -0300

    Updated HomePage.tsx

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
-import VaultTabs from '../components/VaultTabs';
 import ReferralSystem from '../components/ReferralSystem';
 import ProtocolDetails from '../components/ProtocolDetails';
 import InflationComparison from '../components/InflationComparison';
@@ -15,7 +14,6 @@ const HomePage: React.FC = () => {
       <Header />
       <main>
         <Hero />
-        <VaultTabs />
         <HowItWorks />
         <ReferralSystem />
         <ProtocolDetails />
